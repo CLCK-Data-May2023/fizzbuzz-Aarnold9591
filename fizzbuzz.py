1c8b96a8c073014d7f06120164f12c60e271c078
Ashely Arnold <AArnold9591@gmail.com>
2023-08-14 12:59:34 -0400

    Updated to lower case, removed special character

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 
 for i in range(1,101):
     if i % 3 == 0 and i % 5 == 0:
-        print("FIZZBUZZ!")
+        print("fizzbuzz")
     elif i % 3 == 0: 
-            print("Fizz!")
+            print("fizz")
     elif i % 5 == 0:
-        print("Buzz!")
+        print("buzz")
     else: print (i)
